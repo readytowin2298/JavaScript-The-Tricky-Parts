0b740797aa430cd118c06b0e3fba2ae5e767c913
David Ames <readytowin2298@gmail.com>
2021-09-12 23:34:02 -0500

    create-account

--- create-account.js
+++ create-account.js
@@ -1,5 +1,26 @@
 function createAccount(pin, amount) {
-
+    let balance = amount || 0;
+    return {
+        checkBalance : function(code){
+            return code === pin ? `$${balance}` : "Invalid PIN."
+        },
+        deposit : function(code, amt){
+            if(code !== pin) return "Invalid PIN."
+            balance += amt;
+            return `Succesfully deposited $${amt}. Current balance: $${balance}.`;
+        },
+        withdraw : function(code, amt){
+            if(code !== pin) return "Invalid PIN."
+            if(amt > balance) return "Withdrawal amount exceeds account balance. Transaction cancelled."
+            balance -= amt;
+            return `Succesfully withdrew $${amt}. Current balance: $${balance}.`
+        },
+        changePin : function(code, newPin){
+            if(code !== pin) return "Invalid PIN."
+            pin = newPin;
+            return "PIN successfully changed!"
+        }
+    }
 }
 
 module.exports = { createAccount };
